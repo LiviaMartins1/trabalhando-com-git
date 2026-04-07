1f3972ec48eabd24d4476b8df8905f223f1316f8
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-07 16:07:49 -0300

    feat:adicionei ex6

---
+++ ex6.js
@@ -0,0 +1,18 @@
+// const readline = require('readline-sync')
+
+// let meta = 40 
+
+// let horaEstudadas = readline.questionFloat('Quantas horas você estudou?:')
+
+// let diferenca = meta - horaEstudadas
+
+// console.log('======== HORAS ESTUDADAS ========')
+
+// if(meta<= horaEstudadas){
+//     console.log('Parabéns!! Você atingiu a meta')
+// console.log(`Você estudou mais ${Math.abs(diferenca)} horas que a meta`)
+// }else{
+//     console.log(`faltaram ${diferenca} horas`)
+// }
+
+    
