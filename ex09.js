0618e1d2adcd054b1efb802916f37889c0159e9d
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-07 16:09:05 -0300

    feat:adicionei ex9

---
+++ ex09.js
@@ -0,0 +1,15 @@
+// const readline = require('readline-sync')
+
+// let freteGratis = 100
+
+// let valoDaCompra = readline.questionFloat('Qual o valor da compra?')
+
+// let diferenca = freteGratis - valoDaCompra
+
+// if(valoDaCompra > freteGratis){
+//     console.log('Você adquiriu o frete gratis')
+// }else{
+//     console.log('Você não adquiriu o frete gratis')
+//     console.log(`Faltam ${Math.abs(diferenca)} para adquirir o frete gratis`)
+// }
+
