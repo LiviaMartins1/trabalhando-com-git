dab642d6c019b9a6ae72840cc8244965a2aef16c
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-07 16:08:49 -0300

    feat:adicionei ex8

---
+++ ex08.js
@@ -0,0 +1,19 @@
+// const readline= require('readline-sync')
+
+// let idade = readline.questionInt('Informe sua idade:')
+
+// if(idade >= 18){
+//     console.log('Você pode entrar no evento.')
+// }else{
+//     console.log('Você não pode entar no envento')
+// }
+
+
+
+
+
+
+
+
+
+
