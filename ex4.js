36d44cb9755860dc90486ac68b60fb54c406e911
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-07 16:06:20 -0300

    feat:adicionei ex4

---
+++ ex4.js
@@ -0,0 +1,18 @@
+// const readline = require('readline-sync')
+
+
+
+// let meta = 10000
+
+// let passos = readline.questionInt('Quantos passos voce deu hoje?')
+
+// let diferenca = meta - passos 
+
+// console.log('======== RESULTADO ========')
+// if(passos >= meta){
+//     console.log('Parabéns!! Você atingiu a meta')
+//     console.log(`Você passou ${Math.abs (diferenca)} passou da meta`)
+// }else{
+//     console.log(`faltaram ${diferenca} passos`)
+// }
+
