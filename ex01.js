87bae4989b52a65511b313c4248dc114ea9cd166
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-07 15:09:46 -0300

    feat:adicionei ex1

---
+++ ex01.js
@@ -0,0 +1,24 @@
+// const readline = require('readline-sync')
+
+// let nome = readline.question('Digite seu nome: ')
+
+// console.log(`Seu nome é: ${nome}`)
+
+
+//----------------------------------------------//
+
+ 
+// const readline =  require('readline-sync')
+
+// console.log('=======SISTEMA DE CADASTRO========')
+// let nome = readline.question('Digite seu nome:')
+
+// let Curso = readline.question('Informe o curso:')
+
+// let idade = readline.questionInt('Digite sua idade:')
+// console.log(`=======INFORMAÇAO========`)
+// console.log(`Nome do usuario: ${nome}`)
+// console.log(`Curso do usuario: ${Curso}`)
+// console.log(`Idade do usuario: ${idade}`)
+
+
