c40a622590697fe4a1e9d38fb11444ed32b711ab
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-07 16:02:00 -0300

    feat:adicionei ex03

---
+++ ex03.js
@@ -0,0 +1,15 @@
+// const readline = require('readline-sync')
+ 
+// let nome = readline.question('Nickname do Jogador:')
+
+// let jogoFavorito = readline.question('Jogo Favorito:')
+
+// let pontuacaoAtual = readline.question('Pontuação Total:')
+
+// console.log('====== PERFIL DO JOGADOR ======')
+
+// console.log(`Nickname Do Jogador:${nome}`)
+
+// console.log(`Jogo Favorito: ${jogoFavorito}`)
+
+// console.log(`Pontuação Total: ${pontuacaoAtual}`)
