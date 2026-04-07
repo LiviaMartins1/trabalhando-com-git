43deb7c2cc1e47e12f7f9f1a25cc3ac91ee8832a
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-07 16:00:38 -0300

    feat:adicionei e21

---
+++ ex02.js
@@ -0,0 +1,14 @@
+// const readline = require('readline-sync')
+ 
+// console.log('====== SISTEMA DE PEDIDO=======')
+
+// let nome = readline.question('Informe seu nome:')
+ 
+// let nomeDoPrato = readline.question('Informe o nome do prato escolhido:')
+
+// let valorDoPrato = readline.questionFloat('Innforme o valor do prato:')
+
+// console.log('====== RESUMO DO PEDIDO =====')
+// console.log(`Cliente: ${nome}`)
+// console.log(`Prato Escolhido: ${nomeDoPrato}`)
+// console.log(`Valor: ${valorDoPrato}`)
