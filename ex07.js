d21bb53a80ef6412e0666d4fb5b11ccbccd4d06c
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-07 16:08:18 -0300

    feat:adicionei ex7

---
+++ ex07.js
@@ -0,0 +1,14 @@
+// const readline = require ('readline-sync')
+
+// let notaFinal = 7 
+
+// let notaMinima = readline.questionFloat('Informe sua nota:')
+
+// console.log('======== RESULTADO ========')
+
+// if(notaMinima >= notaFinal){
+//     console.log('Você foi aprovado')
+// }else{
+//     console.log('Você nao foi aprovado')
+
+// }
