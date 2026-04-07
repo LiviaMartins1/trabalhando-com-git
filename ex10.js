63202d7d9d6486b7df7bd903d97c6c5c76578ea4
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-07 17:02:48 -0300

    feat:adicionei o ex10

--- ex10.js
+++ ex10.js
@@ -2,11 +2,18 @@ const readline = require ('readline-sync')
 
 let totalDePaginas = 300
 
+
+console.log('------------')
+console.log(`|BIBLIOTECA|`)
+console.log('------------')
+
 let = qtdPaginasLidas = readline.questionInt('Quantas paginas você leu?:')
 
 let percentual = (qtdPaginasLidas / totalDePaginas) * 100
 
+console.log("")
 
 console.log('-------- RESULTADO ---------')
 
+console.log("")
 console.log(`O usuario leu ${percentual}% do livro`)
