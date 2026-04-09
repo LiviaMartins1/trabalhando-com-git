ecd68c2e665ed4d419cd3c890976ce44ef207e5f
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-09 08:01:57 -0300

    feat:adicionei e decorei o ex11

--- ex11.js
+++ ex11.js
@@ -6,4 +6,8 @@ let qtdBaixada = readline.questionInt('Quantos GB foram baixados?:')
 
 let percentual = (qtdBaixada / tamanhoTotal) * 100 
 
+console.log('---------------------------------')
+console.log('---------- RESULTADO ------------')
+console.log('---------------------------------')
+
 console.log(`Foram carregados ${percentual}% do arquivo`)
