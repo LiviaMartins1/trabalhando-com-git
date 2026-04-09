7e626d6800f008bc7625ad558712031590670e88
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-09 07:57:08 -0300

    feat:adicionei o ex11

---
+++ ex11.js
@@ -0,0 +1,9 @@
+const readline = require ('readline-sync')
+
+let tamanhoTotal= 20
+
+let qtdBaixada = readline.questionInt('Quantos GB foram baixados?:')
+
+let percentual = (qtdBaixada / tamanhoTotal) * 100 
+
+console.log(`Foram carregados ${percentual}% do arquivo`)
