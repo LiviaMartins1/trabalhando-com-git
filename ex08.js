cb0e93eb9705226b6976740541422c8366967a67
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-14 08:01:06 -0300

    Update ex08.js

--- ex08.js
+++ ex08.js
@@ -1,12 +1,12 @@
-// const readline= require('readline-sync')
+const readline= require('readline-sync')
 
-// let idade = readline.questionInt('Informe sua idade:')
+let idade = readline.questionInt('Informe sua idade:')
 
-// if(idade >= 18){
-//     console.log('Você pode entrar no evento.')
-// }else{
-//     console.log('Você não pode entar no envento')
-// }
+if(idade >= 18){
+    console.log('Você pode entrar no evento.')
+}else{
+    console.log('Você não pode entar no envento')
+}
 
 
 
