4c7fdce085b56c02d11e22e1f16bb082013c5857
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-14 07:59:56 -0300

    Uncomment user input and output functionality

--- ex01.js
+++ ex01.js
@@ -8,17 +8,17 @@
 //----------------------------------------------//
 
  
-// const readline =  require('readline-sync')
+const readline =  require('readline-sync')
 
-// console.log('=======SISTEMA DE CADASTRO========')
-// let nome = readline.question('Digite seu nome:')
+console.log('=======SISTEMA DE CADASTRO========')
+let nome = readline.question('Digite seu nome:')
 
-// let Curso = readline.question('Informe o curso:')
+let Curso = readline.question('Informe o curso:')
 
-// let idade = readline.questionInt('Digite sua idade:')
-// console.log(`=======INFORMAÇAO========`)
-// console.log(`Nome do usuario: ${nome}`)
-// console.log(`Curso do usuario: ${Curso}`)
-// console.log(`Idade do usuario: ${idade}`)
+let idade = readline.questionInt('Digite sua idade:')
+console.log(`=======INFORMAÇAO========`)
+console.log(`Nome do usuario: ${nome}`)
+console.log(`Curso do usuario: ${Curso}`)
+console.log(`Idade do usuario: ${idade}`)
 
 
