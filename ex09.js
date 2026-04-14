7148083ea988447901ed4015a11549bc177a0b15
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-14 08:01:18 -0300

    Update ex09.js

--- ex09.js
+++ ex09.js
@@ -1,15 +1,15 @@
-// const readline = require('readline-sync')
+const readline = require('readline-sync')
 
-// let freteGratis = 100
+let freteGratis = 100
 
-// let valoDaCompra = readline.questionFloat('Qual o valor da compra?')
+let valoDaCompra = readline.questionFloat('Qual o valor da compra?')
 
-// let diferenca = freteGratis - valoDaCompra
+let diferenca = freteGratis - valoDaCompra
 
-// if(valoDaCompra > freteGratis){
-//     console.log('Você adquiriu o frete gratis')
-// }else{
-//     console.log('Você não adquiriu o frete gratis')
-//     console.log(`Faltam ${Math.abs(diferenca)} para adquirir o frete gratis`)
-// }
+if(valoDaCompra > freteGratis){
+    console.log('Você adquiriu o frete gratis')
+}else{
+    console.log('Você não adquiriu o frete gratis')
+    console.log(`Faltam ${Math.abs(diferenca)} para adquirir o frete gratis`)
+}
 
