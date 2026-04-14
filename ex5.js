35752f62156738e3f41ac4cdf1e4526f36d4f5ad
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-14 08:01:47 -0300

    Update ex5.js

--- ex5.js
+++ ex5.js
@@ -1,15 +1,15 @@
-// const readline = require('readline-sync')
+const readline = require('readline-sync')
 
-// let objetivo = 500
+let objetivo = 500
 
-// let dinheiroGuardado =readline.questionFloat('Quanto dinheiro você guardou esse mês?:')
+let dinheiroGuardado =readline.questionFloat('Quanto dinheiro você guardou esse mês?:')
 
-// let diferenca = objetivo - dinheiroGuardado
+let diferenca = objetivo - dinheiroGuardado
 
-// console.log ('======== DINHEIRO GUARDADO ========')
+console.log ('======== DINHEIRO GUARDADO ========')
 
-// if(objetivo <= dinheiroGuardado){
-//     console.log('Parabéns!! você consegueiu atingir a meta')
-// }else{
-//     console.log(`falta ${diferenca} para alcançar a meta`)
-// }
+if(objetivo <= dinheiroGuardado){
+    console.log('Parabéns!! você consegueiu atingir a meta')
+}else{
+    console.log(`falta ${diferenca} para alcançar a meta`)
+}
