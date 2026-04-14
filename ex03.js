8650c824aaa523776bfeae3c3f087b05dee03a98
LiviaMartins1 <liviaguimaraes611@gmail.com>
2026-04-14 08:00:41 -0300

    Update ex03.js

--- ex03.js
+++ ex03.js
@@ -1,15 +1,15 @@
-// const readline = require('readline-sync')
+const readline = require('readline-sync')
  
-// let nome = readline.question('Nickname do Jogador:')
+let nome = readline.question('Nickname do Jogador:')
 
-// let jogoFavorito = readline.question('Jogo Favorito:')
+let jogoFavorito = readline.question('Jogo Favorito:')
 
-// let pontuacaoAtual = readline.question('Pontuação Total:')
+let pontuacaoAtual = readline.question('Pontuação Total:')
 
-// console.log('====== PERFIL DO JOGADOR ======')
+console.log('====== PERFIL DO JOGADOR ======')
 
-// console.log(`Nickname Do Jogador:${nome}`)
+console.log(`Nickname Do Jogador:${nome}`)
 
-// console.log(`Jogo Favorito: ${jogoFavorito}`)
+console.log(`Jogo Favorito: ${jogoFavorito}`)
 
-// console.log(`Pontuação Total: ${pontuacaoAtual}`)
+console.log(`Pontuação Total: ${pontuacaoAtual}`)
